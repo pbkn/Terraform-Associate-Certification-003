ef91518d4e9cd06a014326080471e959383a1724
Prabhakaran Ravichandran <ravichandran.prabhakaran@gmail.com>
2023-06-10 13:47:18 +0530

    dynamic auth comments

--- first_ec2/first_ec2.tf
+++ first_ec2/first_ec2.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "pbkn-org" #Terraform cloud organization name
     workspaces {
-      name = "tfc-vs-pbkn-pc"
+      name = "tfc-vs-pbkn-pc" #Terraform cloud workspace
     }
   }
   required_providers {
@@ -19,14 +19,15 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
+  # authentication set as env variable in terraform cloud
 }
 
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-049a62eb90480f276"
+  ami           = "ami-049a62eb90480f276" 
   instance_type = "t2.micro"
 
   tags = {
-    "tf" = "ec2-1"
-    Name = "tf-ec2-1"
+    "tf" = "ec2-1" #Custom propery for bill tracking with key "tf"
+    Name = "tf-ec2-1" #EC2 name property
   }
 }
