ffbde5530dfab8bdb132e74c8dc699e409a7a64f
Prabhakaran Ravichandran <ravichandran.prabhakaran@gmail.com>
2023-06-17 18:11:23 +0530

    info link added

--- import/main.tf
+++ import/main.tf
@@ -1,3 +1,6 @@
+#Terraform plan command generates the resource block in terminal window
+#More info https://developer.hashicorp.com/terraform/language/import
+
 resource "aws_instance" "ec2" {
   ami = "" #mandatory param to configure import
   instance_type = "" #mandatory param to configure import
